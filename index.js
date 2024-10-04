d051357ab9b2b96aee54fb70914f4435c51e39cc
lollipopwyn <rwewine@gmail.com>
2024-10-04 12:03:50 +0900

    add userbased recommend

--- index.js
+++ index.js
@@ -145,6 +145,40 @@ app.get('/item-based/:item', (req, res) => {
   });
 });
 
+app.post('/user-based/:item', (req, res) => {
+  const scriptPath = path.join(__dirname, 'recommender.py');
+
+  const inputRatingDict = req.body;
+  const result = spawn(pythonExePath, [scriptPath, 'user-based']);
+
+  let responseData = '';
+
+  // 파이썬 스크립트로 JSON 데이터를 전달
+  result.stdin.write(JSON.stringify(inputRatingDict));
+  result.stdin.end(); // 더 이상 데이터가 없으면 전달 끝
+
+  result.stdout.on('data', function (data) {
+    responseData += data.toString();
+  });
+
+  result.on('close', (code) => {
+    if (code === 0) {
+      try {
+        const jsonResponse = JSON.parse(responseData);
+        res.status(200).json(jsonResponse);
+      } catch (err) {
+        res.status(500).json({ error: 'Failed to parse Python response.' });
+      }
+    } else {
+      res.status(500).json({ error: `Child process exited with code ${code}` });
+    }
+  });
+
+  result.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+});
+
 // 서버 시작
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
